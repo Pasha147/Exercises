db194f3c9aabe26a530072495ef75ca242366f90
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-10-28 15:03:51 +0300

    express 25:00

---
+++ 27_10_2021_ExpresJS_by_Vladilen/index.js
@@ -0,0 +1,53 @@
+import express from "express";
+
+//используется синтаксис с import а не с require
+// поэтому переменную __dirname получаем по другому
+import path from "path";
+
+//используется синтаксис с import а не с require
+// поэтому переменную __dirname получаем по другому
+const __dirname = path.resolve();
+
+//в зависимости от того где запускаем сервер может понадобиться
+// разный порт. process - глобальная переменная в ноде
+// берет PORT или в process.env если есть или если нет то 3000
+const PORT = process.env.PORT ?? 3000;
+// по правилам экспресса нужно проинициализировать приложение
+const app = express();
+
+//концепт middlewear
+//после этого сервер автоматом заходит в static и ищет index.html
+//после этого не нужно прописывать все ответы, сервер будет это понимать по ссылкам
+//автоматом ищет в static
+app.use(express.static(path.resolve(__dirname, "static")));
+
+//23:45 после создания static удаляем
+/*
+//обработать гет запрос по урле "/"
+//Второй параметр можно и function, не обязательно стрелочную
+// в ноде req, res просто объекты. Експр добавляет к ним функционал
+app.get("/", (req, res) => {
+  //16:20 express добавляет хедеры (базовую структуру, кодировку, тип html,...)
+  //можно посмотреть в хроме в нетвоксе
+  // res.send("<h1>Hello Express!!!</h1>");
+
+  //17:40 для ответа файлом а не куском
+  res.sendFile(path.resolve(__dirname, "static", "index.html"));
+});
+*/
+
+//для перехода на features
+//23:45 после создания static удаляем
+/*app.get("/features", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "static", "features.html"));
+});*/
+
+//21:30 обработка пути download
+app.get("/download", (req, res) => {
+  res.download(path.resolve(__dirname, "static", "index.html"));
+});
+
+//метод который запускает сервер (базовый веб сервер)
+app.listen(PORT, () => {
+  console.log(`server has been started on port ${PORT}...`);
+});
