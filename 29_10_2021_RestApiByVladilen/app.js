e14cff63276a4a9fdfdcae7788c63038b852aa32
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-11-02 17:28:51 +0200

    Complited: Rest API NODE Express

--- 29_10_2021_RestApiByVladilen/app.js
+++ 29_10_2021_RestApiByVladilen/app.js
@@ -1,6 +1,43 @@
 const express = require("express");
+const { request } = require("http");
 const path = require("path");
+const { v4 } = require("uuid");
 const app = express(); //создается приложение
+
+let CONTACTS = [{ id: v4(), name: "Pasha", value: "123", marked: false }];
+
+//для работы с json
+app.use(express.json());
+
+//GET
+app.get("/api/contacts", (req, res) => {
+  setTimeout(() => {
+    res.status(200).json(CONTACTS);
+  }, 1000);
+});
+
+//POST
+app.post("/api/contacts", (req, res) => {
+  console.log("req.body>>", req.body);
+  const contact = { ...req.body, id: v4(), marked: false };
+  CONTACTS.push(contact);
+  res.status(201).json(contact);
+});
+
+//DELETE
+//:id динамический параметр
+app.delete("/api/contacts/:id", (req, res) => {
+  CONTACTS = CONTACTS.filter((c) => c.id !== req.params.id);
+  res.status(200).json({ message: "contact has been deleted" });
+});
+
+//PUT
+app.put("/api/contacts/:id", (req, res) => {
+  //   console.log("PUT req", req.params);
+  const idx = CONTACTS.findIndex((c) => c.id === req.params.id);
+  CONTACTS[idx] = req.body;
+  res.json(CONTACTS[idx]);
+});
 
 //-----------------------------
 //это нужно хранить винизу так как эти запросы должны отрабатываться последними
--- 29_10_2021_RestApiByVladilen/client/frontend.js
+++ 29_10_2021_RestApiByVladilen/client/frontend.js
@@ -1,11 +1,21 @@
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js";
-
 // console.log(Vue);
+
+Vue.component("loader", {
+  template: `
+  <div style="display:flex; justify-content:center; align-items:center">  
+  <div class="spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+</div>
+    `,
+});
 
 new Vue({
   el: "#app",
   data() {
     return {
+      loading: false,
       form: {
         name: "",
         value: "",
@@ -13,15 +23,62 @@ new Vue({
       contacts: [],
     };
   },
-  methods: {
-    createContact() {
-      //   const {...contact}=this.form
-      const contact = { ...this.form };
-      //   console.log(contact);
-      this.contacts.push({ ...contact, id: Date.now() });
-
-      this.form.name = this.form.value = "";
-      console.log(this.contacts);
+  computed: {
+    canCreate() {
+      return this.form.value.trim() && this.form.name.trim();
     },
   },
+  methods: {
+    async createContact() {
+      //   const { ...contact } = this.form;
+      const contact = { ...this.form };
+      const newContact = await request("/api/contacts", "POST", contact);
+      console.log("response newContact>>", newContact);
+      //   console.log(contact);
+      this.contacts.push(newContact);
+
+      this.form.name = this.form.value = "";
+      //   console.log(this.contacts);
+    },
+    async markContact(id) {
+      const contact = this.contacts.find((c) => c.id === id);
+      const updated = await request(`/api/contacts/${id}`, "PUT", {
+        ...contact,
+        marked: true,
+      });
+
+      contact.marked = updated.marked;
+    },
+    async removeContact(id) {
+      await request(`/api/contacts/${id}`, "DELETE");
+      this.contacts = this.contacts.filter((c) => c.id !== id);
+    },
+  },
+  async mounted() {
+    this.loading = true;
+    const data = await request("/api/contacts");
+    console.log("data>>", data);
+    this.contacts = data;
+    this.loading = false;
+  },
 });
+//50:00 Универсальный метод запроса
+async function request(url, method = "GET", data = null) {
+  try {
+    const headers = {};
+    let body;
+    if (data) {
+      headers["Content-Type"] = "application/json";
+      body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body,
+    });
+    return await response.json();
+  } catch (error) {
+    console.warn("Error", error.message);
+  }
+}
