7a5a457516cb8657775dc1d83176b920c27129cf
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-12-14 13:56:05 +0200

    typescript and angular

---
+++ 03_11_2021_MyServer/frontend/cv2/src/Main/Main.js
@@ -0,0 +1,12 @@
+function Main() {
+  return (
+    <main className="main">
+      <nav>nav</nav>
+      <article>article</article>
+      <article>article</article>
+      <article>article</article>
+    </main>
+  );
+}
+
+export default Main;
---
+++ 13_12_2021_Angular/first-app/src/app/card/card.component.ts
@@ -0,0 +1,31 @@
+import { PathLocationStrategy } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-card',
+    templateUrl:'./card.component.html',
+    styleUrls:['./card.component.scss'],
+    // interpolation:['{{','}}']
+})
+export class CardComponent implements OnInit {
+
+    title='My Card Title'
+    text='This is my text'
+    number=42
+    obj={name:'Pasha', age:43 }
+
+    getInfo(){
+        return 'this is my info'
+    }
+ 
+ ngOnInit(){
+     setTimeout(()=>{this.imgUrl='./assets/222.svg'},1500)
+     
+ }
+imgUrl='./assets/123.png'
+
+
+}
+
+
+   
