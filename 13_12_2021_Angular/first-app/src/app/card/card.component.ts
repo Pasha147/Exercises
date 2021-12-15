a0ded5100db87a902f65981b012f17da74a8f801
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-12-15 11:51:34 +0200

    angular

--- 13_12_2021_Angular/first-app/src/app/card/card.component.ts
+++ 13_12_2021_Angular/first-app/src/app/card/card.component.ts
@@ -1,31 +1,38 @@
 import { PathLocationStrategy } from '@angular/common';
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-card',
-    templateUrl:'./card.component.html',
-    styleUrls:['./card.component.scss'],
-    // interpolation:['{{','}}']
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  // interpolation:['{{','}}']
 })
 export class CardComponent implements OnInit {
+  title = 'My Card Title';
+  text = 'This is my text';
+  number = 42;
+  obj = { name: 'Pasha', age: 43 };
 
-    title='My Card Title'
-    text='This is my text'
-    number=42
-    obj={name:'Pasha', age:43 }
+  getInfo() {
+    return 'this is my info';
+  }
 
-    getInfo(){
-        return 'this is my info'
-    }
- 
- ngOnInit(){
-     setTimeout(()=>{this.imgUrl='./assets/222.svg'},1500)
-     
- }
-imgUrl='./assets/123.png'
+  ngOnInit() {
+    setTimeout(() => {
+      this.imgUrl = './assets/222.svg';
+    }, 1500);
+  }
+  imgUrl = './assets/123.png';
 
-
+  changeTitle() {
+    this.title = 'Has been changed';
+  }
+  inputHandler(value: any) {
+    //   const value=event.target.value
+      this.title=value
+  }
+  chageHandler(){
+      console.log(this.title);
+      
+  }
 }
-
-
-   
