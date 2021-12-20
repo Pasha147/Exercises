9f564d14bc5f08270daf40bd228c350d1a5b7365
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-12-20 12:08:13 +0200

    19_12_2021 Angular tutorial 4 has been complited

--- 17_12_2021_Angular_from_docs/ang_tutorial/src/app/heroes/heroes.component.ts
+++ 17_12_2021_Angular_from_docs/ang_tutorial/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 // import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,6 +10,13 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
 
   hero:Hero={
     id:1,
@@ -18,6 +26,7 @@ export class HeroesComponent implements OnInit {
   selectedHero?:Hero;
   onSelect(hero: Hero):void{
     this.selectedHero=hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   // heroes=HEROES
@@ -27,11 +36,7 @@ export class HeroesComponent implements OnInit {
   this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes)
   }
 
-  constructor(private heroService: HeroService) { }
 
-  ngOnInit(): void {
-    this.getHeroes()
-  }
-
+ 
 
 }
