9ed8f0a0acfd1a7555c2608efcab20772c03ca37
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-12-23 12:20:31 +0200

    Angular tutorial 5 completed

--- 17_12_2021_Angular_from_docs/ang_tutorial/src/app/hero-detail/hero-detail.component.ts
+++ 17_12_2021_Angular_from_docs/ang_tutorial/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,10 +10,29 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
-@Input() hero?:Hero;
-  constructor() { }
+// @Input() hero?:Hero;
+hero:Hero | undefined  
+constructor(
+    private route: ActivatedRoute, 
+    private heroService: HeroService, 
+    private location: Location,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.getHero()
   }
-
+getHero():void{
+  const id=Number(this.route.snapshot.paramMap.get('id'))
+this.heroService.getHero(id).subscribe(hero=>this.hero=hero)
+}
+goBack():void{
+//  class ro =  Router
+  // this.location.back()
+  // let router: Router = new Router
+  // router.navigateByUrl('/')
+  this.router.navigateByUrl('/')
+ 
+  
+}
 }
