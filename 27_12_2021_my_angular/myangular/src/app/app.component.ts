b1117f4b1d08e5f6e3849cf570a28e04b0761174
Pasha <75729255+Pasha147@users.noreply.github.com>
2022-01-04 15:58:57 +0200

    Angular http client, post requests to heroku are working

--- 27_12_2021_my_angular/myangular/src/app/app.component.ts
+++ 27_12_2021_my_angular/myangular/src/app/app.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
 import { GetpostService } from './getpost.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private getPost: GetpostService){}
+  constructor(private getPost: GetpostService) {}
   title = 'myangular';
-  str:string=""
-  num:number=0
-  clickBtn():void{
-    this.num++
-    this.getPost.getStrO().subscribe(a=>this.str=a)
+  str: string = '';
+  num: number = 0;
+  clickBtn(): void {
     
+
+    this.getPost.getS().subscribe(a=>{
+      this.str+=a
+
+    }
+    )
+
+    
+      
   }
-  
 }
--- 27_12_2021_my_angular/myangular/src/app/getpost.service.ts
+++ 27_12_2021_my_angular/myangular/src/app/getpost.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { interval, Observable, of, Subscriber } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GetpostService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getS(): Observable<string> {
+    const url: string = `https://fpcv.herokuapp.com/api/data/${5}`;
+    let message = { name: 'Pasha', text: 'Hello' };
+    let httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
 
-  getStr():string{
-    return 'aaasss' 
-  }
-
-  getStrO():Observable<string>{
-    const strOb=of ('dddd')
-    return strOb
+    let strO = this.http.post<string>(url, message, httpOptions);
+  
+    return strO;
   }
 }
